2b7f999c2ab27edcd2bb91558465551da3a9d109
IntelliWeb <deventerprisedtb@gmail.com>
2025-07-29 17:52:08 +0200

    FIX: Reestructurar URLs de la API a /api/v1/

--- backend/urls.py
+++ backend/urls.py
@@ -1,14 +1,25 @@
-# backend/urls.py (Modificar)
-
 from django.contrib import admin
 from django.urls import path, include
-from tests.views import CreateCheckoutSessionView # <-- IMPORTA LA NUEVA VISTA
+from tests.views import CreateCheckoutSessionView, StripeWebhookView, EstadisticasUsuarioView
+
+# --- NUEVO: Creamos un conjunto de URLs para nuestra API ---
+api_urlpatterns = [
+    path('', include('tests.urls')), # Incluye /oposiciones, /temas, /preguntas, etc.
+    path('estadisticas/', EstadisticasUsuarioView.as_view(), name='estadisticas-usuario'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tests.urls')),
+    
+    # --- RUTAS DE AUTENTICACIÓN ---
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
-    # --- AÑADE ESTA LÍNEA ---
+    
+    # --- RUTAS DE PAGOS ---
     path('api/create-checkout-session/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('api/webhook/', StripeWebhookView.as_view(), name='stripe-webhook'),
+    
+    # --- RUTA GENERAL PARA NUESTRA API (v1) ---
+    # Todas las rutas de la aplicación (oposiciones, temas, etc.) estarán bajo /api/v1/
+    path('api/v1/', include(api_urlpatterns)),
 ]
