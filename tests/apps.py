b1ca9abe0971ab587f65f27dfcdb36aab1c67164
IntelliWeb <deventerprisedtb@gmail.com>
2025-08-06 21:34:08 +0200

    refactor: Tomar control total de la vista de registro para evtar error 500

--- tests/apps.py
+++ tests/apps.py
@@ -1,6 +1,12 @@
-from django.apps import AppConfig
+# tests/apps.py
 
+from django.apps import AppConfig
 
 class TestsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tests'
+
+    def ready(self):
+        # Importa los módulos de admin aquí para asegurar que Django los descubra.
+        # Esta línea fuerza la carga de nuestro archivo admin.py.
+        import tests.admin
